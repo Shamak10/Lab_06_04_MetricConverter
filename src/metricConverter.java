d95a01f3c4a2dde1180f5ac4042ad3a301165325
Shamak10 <124183731+Shamak10@users.noreply.github.com>
2023-02-17 16:14:08 -0500

    Update metricConverter.java

--- src/metricConverter.java
+++ src/metricConverter.java
@@ -1,5 +1,21 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+public class metricConverter {
+    public static void main(String[] args) {//Creating scanner to take input from the user
+        Scanner scanner = new Scanner("1000");//It is asking for the user input
+        System.out.print("Enter the length in meters: ");
+        if (scanner.hasNextDouble()) {
+            double meters = scanner.nextDouble();//Taking meters as an input
+            if (meters >= 0) {//meter cannot be less than zero
+                double miles = meters / 1609.344;
+                double feet = meters / 0.3048;
+                double inches = meters / 0.0254;
+                System.out.println(meters + " meters");
+                System.out.println(miles + " miles");
+                System.out.println(feet + " feet");
+                System.out.println(inches + " inches");
+            } else {
+                System.err.println("Invalid input. Length cannot be negative.");//Invalid input if the value less than 0
+            }
+        }
     }
 }
